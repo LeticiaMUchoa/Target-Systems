12c028a628f776ad75005ae18df0cafc133fb0b6
LeticiaMUchoa <128520671+LeticiaMUchoa@users.noreply.github.com>
2023-03-22 07:30:36 -0300

    Add files via upload

---
+++ Questão 5/main.py
@@ -0,0 +1,4 @@
+STRING = ('Estou muito feliz que consegui chegar até aqui, foi um desafio para alguem que acabou de começar Python, mas é uma gratificação única poder participar de um processo seletivo da Target Systems.'[::-1])
+
+print (STRING)
+
